b434ebad5e698813358e533ba72e112319f36b96
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-12-04 19:01:29 +0530

    fix: throw if credentials not set

--- zoom_integration/utils.py
+++ zoom_integration/utils.py
@@ -9,6 +9,13 @@ ZOOM_API_BASE_PATH = "https://api.zoom.us/v2"
 
 def authenticate():
 	zoom = frappe.get_single("Zoom Settings")
+
+	if not (zoom.client_id and zoom.get_password(fieldname="client_secret", raise_exception=False) and zoom.account_id):
+		frappe.throw(
+			_("Please set Zoom Client ID, Client Secret and Account ID in Zoom Settings."),
+			title=_("Zoom Settings Incomplete"),
+		)
+
 	authenticate_url = (
 		f"https://zoom.us/oauth/token?grant_type=account_credentials&account_id={zoom.account_id}"
 	)
